a2fcbe9db1977de6b03fbf79a09e45ba7cc457c0
vasemkin <vasemkin@ya.ru>
2023-12-03 15:04:12 +0300

    add curve syscalls usage

--- programs/curves/src/lib.rs
+++ programs/curves/src/lib.rs
@@ -1,23 +1,33 @@
 use anchor_lang::prelude::*;
 
-declare_id!("GiM4xoMCQSCZNioTiALPSYHfhNGCEnKX16gKTK51JbXN"); // Replace with your program's deployed address
+use solana_program::alt_bn128::prelude::{alt_bn128_addition, alt_bn128_multiplication, alt_bn128_pairing};
+
+declare_id!("GiM4xoMCQSCZNioTiALPSYHfhNGCEnKX16gKTK51JbXN"); 
 
 #[program]
 pub mod curves {
     use super::*;
 
-    pub fn bn128_add(_ctx: Context<SumOfElements>, array: Vec<u64>) -> Result<u64> {
-        let element1 = array[0];
-        let element2 = array[1];
-        let sum = element1 + element2;
-        msg!("The sum is: {}", sum);
-        Ok(sum)
-    }
+    pub fn addition(ctx: Context<Bn128>, input: Vec<u8>) -> Result<Vec<u8>>{
+        let result = alt_bn128_addition(&input).unwrap();
+        Ok(result)
+    } 
+    pub fn multiplication(ctx: Context<Bn128>, input: Vec<u8>) -> Result<Vec<u8>>{
+        let result = alt_bn128_multiplication(&input).unwrap();
+        Ok(result)
+    } 
+    pub fn pairing(ctx: Context<Bn128>, input: Vec<u8>) -> Result<Vec<u8>>{
+        let result = alt_bn128_pairing(&input).unwrap();
+        Ok(result)
+    } 
+
 }
 
 #[derive(Accounts)]
-pub struct SumOfElements {}
+pub struct Bn128 {}
 
-
-#[derive(Accounts)]
-pub struct BnAdd {}
+#[error_code]
+pub enum Bn128Error {
+    #[msg("Bn128 error")]
+    MyError
+}
--- tests/verifier.ts
+++ tests/verifier.ts
@@ -3,7 +3,7 @@ import { Program } from '@coral-xyz/anchor'
 //@ts-ignore
 import { Verifier } from '../target/types/verifier_user'
 
-describe.skip('Verifier', () => {
+describe.only('Verifier', () => {
 	const provider = anchor.AnchorProvider.env()
 	anchor.setProvider(provider)
 
@@ -19,10 +19,69 @@ describe.skip('Verifier', () => {
 			.rpc()
 		console.log('Deploy verifier', tx)
 
-		const testGetter = await program.methods
-			.testGetter()
-			.accounts({ dataAccount: dataAccount.publicKey })
+		const tryAddition = await program.methods
+			.tryAddition()
+			.accounts({
+				dataAccount: dataAccount.publicKey,
+				curves_programId: 'GiM4xoMCQSCZNioTiALPSYHfhNGCEnKX16gKTK51JbXN',
+			})
 			.view()
-		console.log({ testGetter })
+		console.log({ tryAddition })
+
+		const tryMul = await program.methods
+			.tryMul()
+			.accounts({
+				dataAccount: dataAccount.publicKey,
+				curves_programId: 'GiM4xoMCQSCZNioTiALPSYHfhNGCEnKX16gKTK51JbXN',
+			})
+			.view()
+		console.log({ tryMul })
+
+		const tryPairing = await program.methods
+			.tryPairing()
+			.accounts({
+				dataAccount: dataAccount.publicKey,
+				curves_programId: 'GiM4xoMCQSCZNioTiALPSYHfhNGCEnKX16gKTK51JbXN',
+			})
+			.view()
+		console.log({ tryPairing })
+
+		const proof = [
+			[
+				'292bc50f76766b6dda7c4b1efb761515c7e56799db65dbcbadf7a0c38cb4e14e',
+				'139fab144a9f42e197eda48d50a7eef4509ed37604e2128504a3b0ad6d76b590',
+			].map((el) => new anchor.BN(el, 'hex')),
+			[
+				[
+					'1e497bfc3db3fcbc6879bc4a7813f6d915a91a3a58b780e0bc31e2e7fcf1246e',
+					'17a2cdb50a3513942a285ae96d329a5b381fb92c9054acacb582097ddf5f886e',
+				].map((el) => new anchor.BN(el, 'hex')),
+				[
+					'21aeafb78df07c3b288edb7570e1ef4a7f57f1e003f58e0c3aa6ccb7a5168980',
+					'16278c77c239b595a803c86c127fd7e8202553474f87b7ace8887b3e79fd9ae2',
+				].map((el) => new anchor.BN(el, 'hex')),
+			],
+			[
+				'101442be94a14ec429292e80faa945d9332ac10b39c768df30141c129e080909',
+				'12b0e1cc424dfb568eb3b88ec8481aba0ac09a5c0c08e6d266954828c0a706d5',
+			].map((el) => new anchor.BN(el, 'hex')),
+			['0000000000000000000000000000000000000000000000000000000000000021'].map(
+				(el) => new anchor.BN(el, 'hex'),
+			),
+		]
+
+		// console.log({ proof })
+
+		console.log(program.methods)
+
+		const verificationResult = await program.methods
+			.verifyProof(...proof)
+			.accounts({
+				dataAccount: dataAccount.publicKey,
+				curves_programId: 'GiM4xoMCQSCZNioTiALPSYHfhNGCEnKX16gKTK51JbXN',
+			})
+			.view()
+
+		console.log({ verificationResult })
 	})
 })
